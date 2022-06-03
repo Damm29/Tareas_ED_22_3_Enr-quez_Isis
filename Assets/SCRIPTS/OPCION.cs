6a674e9c67d73c002f0d404025e63edb946cf675
Damm <106337457+Damm29@users.noreply.github.com>
2022-06-03 13:06:24 -0500

    INPUT OPTION

--- Assets/SCRIPTS/OPCION.cs
+++ Assets/SCRIPTS/OPCION.cs
@@ -5,8 +5,14 @@ using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    public TextMeshProUGUI usser_option;
+    public TMP_InputField usser_input;
+
     public void opcionmusical()
     {
+
+        usser_option.text = usser_input.text;
 
     }
 }
