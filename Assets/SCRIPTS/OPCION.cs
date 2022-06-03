3eef3968d4b606fd3c9ea26bb4c4a98f59e3c7c0
Damm <106337457+Damm29@users.noreply.github.com>
2022-06-03 13:45:58 -0500

    Tiene codigo de opciones, no se puede enlazar

--- Assets/SCRIPTS/OPCION.cs
+++ Assets/SCRIPTS/OPCION.cs
@@ -8,22 +8,29 @@ public class NewBehaviourScript : MonoBehaviour
 
     public TextMeshProUGUI usser_option;
     public TMP_InputField usser_input;
-    char A;
-    char B;
-    char C; 
-    char D; 
-
+   
     public void opcionmusical()
     {
-
-      
+      if (Input.GetKeyDown(KeyCode.A))
+      {
+          usser_input.text="Tu canción es Psycho Killer";
+      }
+      if (Input.GetKeyDown(KeyCode.B))
+      {
+          usser_input.text="Tu canción es Lullaby";
+      }
+      if (Input.GetKeyDown(KeyCode.C))
+      {
+          usser_input.text="Tu canción es Dear God";
+      }
+      if (Input.GetKeyDown(KeyCode.D))
+      {
+          usser_input.text="Tu canción es Smells Like Teen Spirit";
+      }
 
     }
-
-    public void Reset()
+    public void Reset() 
     {
-        
-
-
+        usser_input.text="";
     }
 }
